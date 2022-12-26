a411b8ecd3ef2af401c17454862a282a555c3c18
Đỗ Trọng Anh Tuấn (BU01 PTPM) <tuandta2@viettel-softwareservices.vn>
2022-12-26 10:16:44 +0700

    fix token verify when build

--- routes/voucher.js
+++ routes/voucher.js
@@ -4,7 +4,7 @@ const middlewareController = require("../controllers/middlewareController");
 const router = require('express').Router();
 
 router.post('/createVoucher', middlewareController.veryfyToken, voucherController.createVoucher);
-router.get('/getAllVoucher', middlewareController.veryfyToken, voucherController.getAllVoucher);
+router.get('/getAllVoucher', voucherController.getAllVoucher);
 router.delete('/:id', middlewareController.veryfyToken,voucherController.deleteVoucher);
 router.put('/getvoucher/:id', middlewareController.veryfyToken,voucherController.updateVoucher);
 router.get('/getUserId/:id', middlewareController.veryfyToken, voucherController.getUserId)
